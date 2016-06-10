d0d69ff69931fdd6f45d636b979a7e6ee3615c17
jmclean2 <james@svds.com>
2016-06-10 11:05:24 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,13 @@ from slacker import Slacker
 from channel import Channel
 from user import User
 from message import Message
+from model import slack_user, message_channel, message
 
-from slack_entries_database import slack_user, message_channel, message
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI']
 slackconnect = Slacker(app.config["slack_api_token"])
 
